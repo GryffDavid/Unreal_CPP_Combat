42778e5fd1bc9eda886dd9c4c9d1f7576525300e
Gryff David <gryffdavid@gmail.com>
2021-01-03 21:33:42 +0200

    Finally got something kinda working after a tonne of iterations trying to get the behaviour I want. It's very simple at the moment. I tried to make it more complex but easier to use in the long-run and it didn't go well. I still want to move it over to C++ if possible.

---
+++ Source/Unreal_CPP_Combat/InteractiveComponent.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/PrimitiveComponent.h"
+#include "InteractiveComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL_CPP_COMBAT_API UInteractiveComponent : public UPrimitiveComponent
+{
+	GENERATED_BODY()
+	
+	protected:
+
+	public:
+		UInteractiveComponent();
+	
+	private:
+};
