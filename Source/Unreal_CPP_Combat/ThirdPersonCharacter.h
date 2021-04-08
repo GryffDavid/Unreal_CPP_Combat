485864e3d329ed73e9384b9e7fe9b9d8703b9a70
Gryff David <gryffdavid@gmail.com>
2021-04-08 20:16:25 +0200

    Started again on creating the system for party members being able to be possessed by the player.

--- Source/Unreal_CPP_Combat/ThirdPersonCharacter.h
+++ Source/Unreal_CPP_Combat/ThirdPersonCharacter.h
@@ -26,7 +26,7 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
+	
 	UPROPERTY(BlueprintReadWrite, Category = Health)
 	int CurrentHealth = 10;
 
